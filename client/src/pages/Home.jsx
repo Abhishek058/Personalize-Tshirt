ce96589fefc14d3d6211b5ddb833b7061e04b46b
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-08-18 23:03:00 +0530

    content switching code completed with minor bug fixes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,15 +15,15 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
-          <motion.header>
+          <motion.header {...slideAnimation("down")}>
             <img
               src="./threejs.png"
               alt="logo"
               className="w-8 h-8 object-contain"
             />
           </motion.header>
-          <motion.div className="home-content">
-            <motion.div {...headContentAnimation}>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
               <h1 className="head-text">
                 LET'S
                 <br className="xl:block hidden" /> DO IT.
@@ -42,7 +42,7 @@ const Home = () => {
                 type="filled"
                 title="Customize It"
                 handleClick={() => (state.intro = false)}
-                customStyles="w-fit px-4 py-2.5 font-bold text-sm"  
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
             </motion.div>
           </motion.div>
